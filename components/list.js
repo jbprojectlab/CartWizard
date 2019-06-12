cc51ed9ab4df1c0a95311a56dbb53a87ef83ce5c
James Byrd <James.Byrd1265@gmail.com>
2019-06-12 15:05:41 -0400

    chore: integrate firestore

--- components/list.js
+++ components/list.js
@@ -1,24 +1,52 @@
-import React, {Component} from 'react';
-import {SectionList, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react'
+import firebase from 'react-native-firebase'
+import {SectionList, StyleSheet, Text, View} from 'react-native'
 
-var List = function() {
-  return (
-    <SectionList
-      renderItem={({item, index, section}) => <Text key={index}>{item}</Text>}
-      renderSectionHeader={({section: {title}}) => (
-        <Text style={{fontWeight: 'bold'}}>{title}</Text>
-      )}
-      sections={[
-        {title: 'General', data: ['KIND Bars', 'Frosted Flakes']},
-        {title: 'Produce', data: ['Oranges', 'Green Bell Peppers']},
-        {title: 'Meat', data: ['Ground Chuck', 'Mahi']}
-      ]}
-      keyExtractor={function(item, index) {
-        return item + index
-      }}
-    />
-  )
-};
+const filterItems = (items, section) => {
+  return items.map(item => {
+    if(item._data.section === section) return item.id
+  })
+}
+
+class List extends Component {
+  state = {
+    items: []
+  }
+
+  async componentDidMount() {
+    const items = []
+    try {
+      const itemData = await firebase.firestore().collection('items').get()
+      itemData.forEach(item => items.push(item))
+      this.setState({items})
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+  render() {
+    const {items} = this.state
+    const produce = filterItems(items, 'produce')
+    const meat = filterItems(items, 'meat')
+    return (
+      <SectionList
+        renderItem={({item, index, section}) => <Text key={index}>{item}</Text>}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={{fontWeight: 'bold'}}>{title}</Text>
+        )}
+        sections={[
+          {title: 'General', data: []}, //pull data from where section === section
+          {title: 'Produce', data: produce},
+          {title: 'Meat/Poultry', data: meat}
+        ]}
+        keyExtractor={function(item, index) {
+          return item + index
+        }}
+      />
+    )
+  }    
+}
+
 
 var styles = StyleSheet.create({
   category: {
